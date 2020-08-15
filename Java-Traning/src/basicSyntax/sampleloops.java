575f18292f9b95629e1a39db2ee598b9676401e7
muleraja01 <69101219+muleraja01@users.noreply.github.com>
2020-08-15 12:37:51 +0530

    Added new sample Programms

--- Java-Traning/src/basicSyntax/sampleloops.java
+++ Java-Traning/src/basicSyntax/sampleloops.java
@@ -1,9 +1,21 @@
 package basicSyntax;
 
 public class sampleloops {
+	/**
+	 * @param sample loops
+	 */
 	public static void main(String[] args) {
 		for (int i = 1; i < 10; i++) {
 			System.out.println(i);
+			if (i != 4) {
+				System.out.println(i);
+			}
+		}
+		int j = 0;
+		while (j <= 10) {
+			System.out.println(j);
+			j++;
+
 		}
 	}
 }
