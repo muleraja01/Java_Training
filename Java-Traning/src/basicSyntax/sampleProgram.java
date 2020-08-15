3fe0ba4e91527129f58926da82cacfe56d2bd079
muleraja01 <69101219+muleraja01@users.noreply.github.com>
2020-08-15 11:38:22 +0530

    adding some more sample programms

---
+++ Java-Traning/src/basicSyntax/sampleProgram.java
@@ -0,0 +1,23 @@
+package basicSyntax;
+
+public class sampleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sample print java programing");
+		int a=10;
+		int b=10;
+		System.out.println("addition of Two Numbers "+(a+b));
+		
+		int c=10;
+		char d='A';
+		double e=123.456;
+		boolean f=true;
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		
+	}
+
+}
